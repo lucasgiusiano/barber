9098bde637c389ab4409fa3b810d109ba75e575b
lucasgiusiano <luucass200026@gmail.com>
2022-11-18 01:56:52 -0300

    refresh

--- js/index.js
+++ js/index.js
@@ -3,5 +3,9 @@
 document.addEventListener("DOMContentLoaded",()=>{
     document.getElementById("horarios").addEventListener("click", ()=>{
         window.location = "horarios.html"
-    })
+    });
+
+    document.getElementById("reservar").addEventListener("click",()=>{
+        window.location  = "horarios.html"
+    });
 })
